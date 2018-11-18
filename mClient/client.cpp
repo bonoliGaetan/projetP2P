fbef884f708756ff2220d18c40bd0fba626e145e
Woshis <gnbprog@gmail.com>
2018-11-18 14:46:21 +0100

    Ajout de l ecriture du fichier liste de pair

--- mClient/client.cpp
+++ mClient/client.cpp
@@ -5,11 +5,6 @@ std::vector<Peer> Client::get_pair_liste()
 	return pairListe;
 }
 
-void Client::sauvegarde_fichier(std::string param)
-{
-	;
-}
-
 void Client::init_pair_liste()
 {
 	// Ouverture du fichier contenant la liste des pairs.
@@ -42,5 +37,46 @@ void Client::init_pair_liste()
 
 void Client::ajout_pair_liste(std::vector<Peer> liste)
 {
-	;
+	int estDansListe = 0;
+	
+	for(unsigned int i = 0; i < liste.size(); i++)
+	{
+		for(unsigned int j = 0; j < pairListe.size(); j++)
+		{
+			if(liste[i].url == pairListe[j].url)
+			{
+				estDansListe = 1;
+				break;
+			}
+			
+		}
+		
+		if(estDansListe == 0)
+		{
+			std::cout<< liste[i].url << " A ETE AJOUTER"<<std::endl;
+			pairListe.push_back(liste[i]);
+		}
+		else
+		{
+			std::cout<< liste[i].url << " est deja dans votre liste"<<std::endl;
+			estDansListe = 0;
+		}
+	}
+}
+
+void Client::ecriture_fichier_liste_pair()
+{
+	std::ofstream fichier("listePaire.txt");
+	
+	if(fichier)
+	{
+		for(unsigned int i = 0; i < pairListe.size(); i++)
+		{
+			fichier<< pairListe[i].url << " " << pairListe[i].nom<<std::endl;
+		}
+	}
+	else
+	{
+		std::cout<<"Probleme lors de l'ouverture du fichier"<<std::endl;
+	}
 }
--- mClient/client.h
+++ mClient/client.h
@@ -13,12 +13,9 @@ class Client
 	// TODO => Liste d'option
 	
 	public:
-	void sauvegarde_fichier(std::string param);
-	std::vector<Peer> get_pair_liste();
-	void init_pair_liste();
-	void ajout_pair_liste(std::vector<Peer> liste);
-	// TODO => Faire une fct qui prend un fichier texte, qui lit et qui ecrit le contenue du txt dans pairListe 
-	// TODO => Faire une fct qui prend un pairListe puis une autre liste de pair, qui enregistre dans pairListe les 
-	// nouveau pair, et qui met a jour la liste des pair dans le fichier texte (tous les nouveau pair sont ajoutes 
-	// dans la liste du fichier texte).
+	// MAYBY TODO => void sauvegarde_fichier(std::string param);
+	std::vector<Peer> get_pair_liste(); // getter
+	void init_pair_liste(); // Prend le fichier contenant la liste des pair, et remplie pairListe
+	void ajout_pair_liste(std::vector<Peer> liste); // Ajoute tous ce qui est dans liste et qui n'est pas dans pairListe
+	void ecriture_fichier_liste_pair();
 };
