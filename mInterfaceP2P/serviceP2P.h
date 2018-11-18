5aa85c2c46f7c2acf3a9477f5cd1a6e0235ee0b6
Kryomix <maxibin78@gmail.com>
2018-11-18 15:00:23 +0100

    maj serviceP2P

--- mInterfaceP2P/serviceP2P.h
+++ mInterfaceP2P/serviceP2P.h
@@ -1,17 +1,54 @@
 #include <iostream>
 #include <string>
-#include <list>
+#include <vector>
+
+// Include REST API
+
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+#include <cpprest/http_listener.h>              // HTTP server
+#include <cpprest/json.h>                       // JSON library
+
+using namespace utility;                    	// Common utilities like string conversions
+using namespace web;                        	// Common features like URIs.
+using namespace web::http;                  	// Common HTTP functionality
+using namespace web::http::client;          	// HTTP client features
+using namespace concurrency::streams;       	// Asynchronous streams
+using namespace web::http::experimental::listener;          // HTTP server
+using namespace web::experimental::web_sockets::client;     // WebSockets client
+using namespace web::json;                                  // JSON library
+
+// ***************
 
 #include "../mDataClass/file.h"
 #include "../mDataClass/peer.h"
 
+#define LOCALHOST "127.0.0.1"
+
+
+typedef struct
+{
+	int code;
+	std::string codeString;
+	std::string message;
+}StError;
+
+StError new_StError();
 
 class ServiceP2P 
 {
 public:
+
+	ServiceP2P();
+	~ServiceP2P();
+
+	StError lastError;
+
+	void test();
+
 	// CLIENT
-	std::list<Peer> GetPeerList(std::string url);
-	std::list<File> GetFileList(std::string url);
+	std::vector<Peer> GetPeerList(std::string url);
+	std::vector<File> GetFileList(std::string url);
 	void GetFile(std::string url, File &file);
 	
 	void DeleteFile(std::string url, std::string id);
@@ -19,24 +56,26 @@ public:
 	void UpdateDataFile(std::string url, File file);
 	void UpdateMetaFile(std::string url, File file);
 
-	Peer WaitRegister(int* typeReg);
+	void WaitRequestClient(int* typeReq, void* data);
 	
 	// SERVEUR
 	void registerPeer(std::string url);
 	void unregisterPeer(std::string url);
 
-	void SendPeerList(std::string url, std::list<Peer>);
-	void SendFileList(std::string url, std::list<File>);
+	void SendPeerList(std::string url, std::vector<Peer>);
+	void SendFileList(std::string url, std::vector<File>);
 
 	void SendMetaFile(std::string url, File file);
 	void SendDataFile(std::string url, File file);
 
+	void WaitRequestServer(int* typeReq, void* data);
+
 protected:
 
 	// Variables
-	// attibuts peut être nécessaire pour les requêtes http
-	// Si oui, fait un contructeur
-	// Si non, mettre en static
+	std::string addrIP;
+	int outPort;
+	int inPort;
 
 
 	// Fonction d'écriture de requêtes
@@ -49,8 +88,8 @@ protected:
 
 	std::string WRRegisterPeer(std::string url);
 	std::string WRUnregisterPeer(std::string url);
-	std::string WRSendPeerList(std::string url, std::list<Peer>);
-	std::string WRSendFileList(std::string url, std::list<File>);
+	std::string WRSendPeerList(std::string url, std::vector<Peer>);
+	std::string WRSendFileList(std::string url, std::vector<File>);
 	std::string WRSendDataFile(std::string url, File file);
 	std::string WRSendMetaFile(std::string url, File file);
 
