46ed5ec3cf23a7c6a0b407eed362195327f42899
Woshis <gnbprog@gmail.com>
2018-11-19 21:50:35 +0100

    Maj du client (suppr fichier peer) (ajout fichier liste option)

--- mClient/client.cpp
+++ mClient/client.cpp
@@ -2,39 +2,57 @@
 
 Client::Client()
 {
-	init_pair_liste();
+	std::cout<<"DEBUT"<<std::endl;
+	init_liste_option();
 }
 
 Client::~Client()
 {
-	ecriture_fichier_liste_pair();
+	std::cout<<"FIN"<<std::endl;
 }
 
-std::vector<Peer> Client::get_pair_liste()
+
+void Client::requete_au_client()
 {
-	return pairListe;
+	// fonction wait
+	// traiter ce que l'on a recu de la fonction wait
 }
 
-void Client::init_pair_liste()
+void Client::init_liste_option()
 {
 	// Ouverture du fichier contenant la liste des pairs.
-	std::ifstream fichier("listePaire.txt",std::ifstream::in);
+	std::ifstream fichier("listeOption.txt",std::ifstream::in);
 	
 	if(fichier)
 	{
-		std::cout<<"Le fichier listePaire.txt s'est ouvert"<<std::endl;
+		std::cout<<"Le fichier listeOption.txt s'est ouvert"<<std::endl;
 		
-		std::string url;
-		std::string nom;
+		std::string clef;
+		std::string egale;
+		std::string valeur;
 		
-		while(fichier >> url && fichier >> nom) // On lis tout le fichier
+		while(fichier >> clef && fichier >> egale) // On lis tout le fichier
 		{	
 			// On enregistre contenue dans la liste des pair
-			Peer pair;
-			pair.url = url;
-			pair.nom = nom;
-			
-			pairListe.push_back(pair);
+			if(egale == "==")
+			{
+				std::string valeurFinal = "";
+				
+				while(fichier >> valeur)
+				{
+					if(valeur == "\"") break;
+					
+					if(valeurFinal == "") valeurFinal = valeurFinal + valeur;
+					
+					else valeurFinal = valeurFinal + " " + valeur;
+				}
+				listeOption[clef] = valeurFinal;
+			}
+			else
+			{
+				fichier >> valeur;
+				listeOption[clef] = valeur;
+			}
 		}
 		
 		fichier.close();
@@ -45,48 +63,13 @@ void Client::init_pair_liste()
 	}
 }
 
-void Client::ajout_pair_liste(std::vector<Peer> liste)
+void Client::changer_liste_option(std::string nomOption,std::string nouvelleValeur)
 {
-	int estDansListe = 0;
-	
-	for(unsigned int i = 0; i < liste.size(); i++)
-	{
-		for(unsigned int j = 0; j < pairListe.size(); j++)
-		{
-			if(liste[i].url == pairListe[j].url)
-			{
-				estDansListe = 1;
-				break;
-			}
-			
-		}
-		
-		if(estDansListe == 0)
-		{
-			std::cout<< liste[i].url << " A ETE AJOUTER"<<std::endl;
-			pairListe.push_back(liste[i]);
-		}
-		else
-		{
-			std::cout<< liste[i].url << " est deja dans votre liste"<<std::endl;
-			estDansListe = 0;
-		}
-	}
+	listeOption.erase(nomOption);
+	listeOption[nomOption] = nouvelleValeur;
 }
 
-void Client::ecriture_fichier_liste_pair()
+std::map<std::string,std::string> Client::get_liste_option()
 {
-	std::ofstream fichier("listePaire.txt");
-	
-	if(fichier)
-	{
-		for(unsigned int i = 0; i < pairListe.size(); i++)
-		{
-			fichier<< pairListe[i].url << " " << pairListe[i].nom<<std::endl;
-		}
-	}
-	else
-	{
-		std::cout<<"Probleme lors de l'ouverture du fichier"<<std::endl;
-	}
+	return listeOption;
 }
--- mClient/client.h
+++ mClient/client.h
@@ -1,7 +1,10 @@
+#ifndef CLIENT_H
+#define CLIENT_H
 #include<iostream>
 #include<string>
 #include<vector>
 #include<fstream>
+#include<map>
 
 #include"../mDataClass/peer.h"
 
@@ -9,15 +12,17 @@ class Client
 {
 	private:
 	std::string url;
-	std::vector<Peer> pairListe;
-	// TODO => Liste d'option
+	std::map<std::string,std::string> listeOption;
+	std::vector<Peer> listePaire;
+	// TODO => fonction dans un thread qui utilise waitRequestedClient et quiva traiter les donnes recues.
 	
 	public:
 	Client();
 	~Client();
-	// MAYBY TODO => void sauvegarde_fichier(std::string param);
-	std::vector<Peer> get_pair_liste(); // getter
-	void init_pair_liste(); // Prend le fichier contenant la liste des pair, et remplie pairListe
-	void ajout_pair_liste(std::vector<Peer> liste); // Ajoute tous ce qui est dans liste et qui n'est pas dans pairListe
-	void ecriture_fichier_liste_pair();
+	// MAYBY TODO => void sauvegarde_fichier(std::string chemin);
+	
+	std::map<std::string,std::string> get_liste_option(); // getter
+	void init_liste_option(); // lis le fichier de la liste des option
+	void changer_liste_option(std::string nomOption,std::string nouvelleValeur);
 };
+#endif
