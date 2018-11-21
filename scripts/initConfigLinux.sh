b8ac902bc67c3b4783cb57527f73f0f54258aa21
Kryomix <maxibin78@gmail.com>
2018-11-22 00:18:14 +0100

    Ajout ReadMe

--- scripts/initConfigLinux.sh
+++ scripts/initConfigLinux.sh
@@ -3,7 +3,7 @@ ADDR=`ip addr | grep 'state UP' -A2 | tail -n1 | awk -F'[/ ]+' '{print $3}'`
 echo 'Repertoire_Client == SavedFileClient "' > listeOption.txt
 echo 'Repertoire_Server == SavedFileServer "' >> listeOption.txt
 echo "Taille_max_repertoire_Serveur = 1000000000" >> listeOption.txt
-echo "Ip_init_Serveur = 192.168.0.1" >> listeOption.txt
+echo "Ip_init_Serveur = ${1}" >> listeOption.txt
 echo 'Ip_address_client = ${ADDR}'
 echo "Port_min = 5000" >> listeOption.txt
 echo "Port_max = 5010" >> listeOption.txt
